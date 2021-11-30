fe68b21f36882a1632431f9b1eac5401655fec2a
Pandeo_F1 <58373303+PandeoF1@users.noreply.github.com>
2021-11-30 16:33:57 +0100

    clean

--- so_long.c
+++ so_long.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tnard <tnard@student.42lyon.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/26 17:38:27 by tnard             #+#    #+#             */
+/*   Updated: 2021/11/29 03:01:34 by tnard            ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "includes/so_long.h"
 
 int	main(int argc, char **argv)
@@ -14,11 +26,11 @@ int	main(int argc, char **argv)
 		if (ft_check_file(argv[1]) && ft_init_map(&game, argv[1]))
 		{
 			game->player_mouv = 0;
-			ft_printf("\n\ngg\n\n");
 			mlx_key_hook(game->mlx_win, ft_win_event, &game);
 			mlx_hook(game->mlx_win, 17, 1L << 17, ft_exit_hook, &game);
 			mlx_loop(game->mlx);
 			ft_close(&game);
+			ft_printf("You made : %i.\n", game->player_mouv);
 		}
 		else
 			ft_printf("Error\n");
