29f8d858e17c490c9eb4a6725188025958274aec
Nard Theo <theo.nard18@gmail.com>
2021-12-10 22:36:12 +0100

    g

--- srcs/ft_init_map.c
+++ srcs/ft_init_map.c
@@ -6,7 +6,7 @@
 /*   By: tnard <tnard@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/26 17:39:52 by tnard             #+#    #+#             */
-/*   Updated: 2021/12/10 22:35:14 by tnard            ###   ########lyon.fr   */
+/*   Updated: 2021/12/10 22:36:06 by tnard            ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,7 @@ int	ft_push_img(t_so_long **game, int x, int y)
 	{
 		mlx_put_image_to_window((*game)->mlx, (*game)->mlx_win,
 			(*game)->player[0], x * (*game)->mult, y * (*game)->mult);
-		printf("test1\n");
+		printf("test1 : %c\n", (*game)->str[y][x]);
 		if ((*game)->player_x != 0 && (*game)->player_y != 0)
 			return (0);
 		printf("test2\n");
